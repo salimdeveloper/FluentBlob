cf3928e2b298f821a1c86079c00110697dc84194
salim alam <salimalamdeveloper@gmail.com>
2019-12-10 16:18:44 +0000

    Implemented GetAllFilesmethod

--- src/FluentBlob.Core/BlobService.cs
+++ src/FluentBlob.Core/BlobService.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace FluentBlob.Core
 {
+    /// <author>
+    /// SALIM ALAM
+    /// DATE: 12/10/2019
+    /// https://github.com/salimdeveloper
+    /// </author>
     public sealed class BlobService:IBlobActions,IContainerActions
     {
         private readonly string _connectionString;
-        private object _containerName;
+        private string _containerName;
 
         public BlobService(string connectionString) => 
             this._connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
@@ -24,22 +30,75 @@ namespace FluentBlob.Core
         {
             throw new NotImplementedException();
         }
-        public IFileReadActions Download(string fileName)
-        {
-            throw new NotImplementedException();
-        }
-        public IEnumerable<IListBlobItem> GetAllFiles()
-        {
-            throw new NotImplementedException();
-        }
-        public string GetSharedUrl(string fileName, int sharedAccessMinutes)
-        {
-            throw new NotImplementedException();
-        }
         public IFileWriteActions Upload(string fileName)
         {
             throw new NotImplementedException();
         }
+        public IFileReadActions Download(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Gets all blob items in a container
+        /// </summary>
+        /// <returns>IEnumerable<IListBlobItem></returns>
+        public IEnumerable<IListBlobItem> GetAllFiles()
+        {
+            CloudBlobContainer _blobContainer = GetBlobContainer();
+            BlobContinuationToken _continuationToken = null;
+            do
+            {
+                var _response = _blobContainer.ListBlobsSegmentedAsync(string.Empty,true,BlobListingDetails.All,new int?()
+                    ,_continuationToken,null,null);
+                foreach (var blob in _response.Result.Results)
+                {
+                    yield return blob;
+                }
+            } while (_continuationToken!=null);
+        }
+        /// <summary>
+        /// Gets blob uri with shared access token.
+        /// </summary>
+        /// <param name="fileName">String</param>
+        /// <param name="sharedAccessMinutes">int</param>
+        /// <returns></returns>
+        public string GetSharedUri(string fileName, int sharedAccessMinutes)
+        {
+            CloudBlobContainer _blobContainer = GetBlobContainer();
+            CloudBlockBlob _cloudBlob = _blobContainer.GetBlockBlobReference(fileName);
+            string _sasBlobToken = GetSharedAccessToken(_cloudBlob, sharedAccessMinutes);
+            return _cloudBlob.Uri + _sasBlobToken;
+        }
+
+        #region Helper Methods
+        /// <summary>
+        /// Initializes SharedAccess,Sets Access Time and Permission
+        /// </summary>
+        /// <param name="blob">CloudBlockBlob</param>
+        /// <param name="sharedAccessMinutes">int</param>
+        /// <returns>Blob Token</returns>
+        private static string GetSharedAccessToken(CloudBlockBlob blob, int sharedAccessMinutes)
+        {
+            var sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-1);
+            sasConstraints.SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(sharedAccessMinutes);
+            sasConstraints.Permissions = SharedAccessBlobPermissions.Read;
+
+            var sasBlobToken = blob.GetSharedAccessSignature(sasConstraints);
+            return sasBlobToken;
+        }
+        /// <summary>
+        /// Gets Initialized Blob Container
+        /// </summary>
+        /// <returns>CloudBlobContainer</returns>
+        private CloudBlobContainer GetBlobContainer()
+        {
+            CloudBlobClient _serviceClient = CloudStorageAccount.Parse(this._connectionString).CreateCloudBlobClient();
+            var container = _serviceClient.GetContainerReference(this._containerName);
+            return container;
+        }
+        #endregion
+
     }
 
 
