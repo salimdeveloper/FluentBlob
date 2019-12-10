f6d40568ef453907b5eabbf7e4c1318c749f2ac7
salim alam <salimalamdeveloper@gmail.com>
2019-12-10 13:40:12 +0000

    Added method sign to delete file in container by filename

--- src/FluentBlob.Core/Interfaces/IContainerActions.cs
+++ src/FluentBlob.Core/Interfaces/IContainerActions.cs
@@ -5,11 +5,16 @@ using System.Text;
 
 namespace FluentBlob.Core
 {
+    /// <author>
+    /// SALIM ALAM
+    /// DATE: 12/10/2019
+    /// https://github.com/salimdeveloper
+    /// </author>
     public interface IContainerActions
     {
         IFileReadActions Download(string fileName);
         string GetSharedUrl(string fileName, int sharedAccessMinutes);
-
+        void Delete(string fileName);
         IEnumerable<IListBlobItem> GetAllFiles();
     }
 }
