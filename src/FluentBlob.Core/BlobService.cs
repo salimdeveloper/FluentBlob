b93cd2269743bbfe5b69f2f71d6ae1a11a0300d8
salim alam <salimalamdeveloper@gmail.com>
2019-12-10 14:18:18 +0000

    Added Constructor , takes azure blob storage connectionstring

--- src/FluentBlob.Core/BlobService.cs
+++ src/FluentBlob.Core/BlobService.cs
@@ -4,7 +4,11 @@ namespace FluentBlob.Core
 {
     public sealed class BlobService
     {
+        private readonly string _connectionString;
 
+        public BlobService(string connectionString) => 
+            this._connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+      
     }
 
 
