4babd416e191bed39931f7ac7618f68ff46043d9
salim alam <salimalamdeveloper@gmail.com>
2019-12-10 14:27:51 +0000

    Added Connect method to statically initialize BlobService, Implemented IBlobActions Interface

--- src/FluentBlob.Core/BlobService.cs
+++ src/FluentBlob.Core/BlobService.cs
@@ -2,13 +2,25 @@
 
 namespace FluentBlob.Core
 {
-    public sealed class BlobService
+    public sealed class BlobService:IBlobActions
     {
         private readonly string _connectionString;
 
         public BlobService(string connectionString) => 
             this._connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
-      
+
+        public static IBlobActions Connect(string connectionString) =>
+           new BlobService(connectionString);
+
+        public IContainerActions GetContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContainerActions SetContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
     }
 
 
