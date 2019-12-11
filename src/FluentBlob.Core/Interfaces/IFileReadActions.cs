611dc3ec0d4947055bd2824889f4f628dc9da2fa
salim alam <salimalamdeveloper@gmail.com>
2019-12-11 20:30:34 +0000

    Commented ToFileMethod

--- src/FluentBlob.Core/Interfaces/IFileReadActions.cs
+++ src/FluentBlob.Core/Interfaces/IFileReadActions.cs
@@ -9,7 +9,7 @@ namespace FluentBlob.Core
     /// </author>
     public interface IFileReadActions
     {
-        void ToFile(string filePath);
+        //void ToFile(string filePath);
         void ToStream(Stream stream);
     }
 }
