d43c05bb3f46654d9784358973ba95573757b24d
salim alam <salimalamdeveloper@gmail.com>
2019-12-19 12:59:06 +0000

    Custom exception for blob not found added

---
+++ src/FluentBlob.Core/BlobNotFoundException.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FluentBlob.Core
+{
+    class BlobNotFoundException : StorageException
+    {
+        public BlobNotFoundException()
+        {
+        }
+
+        public BlobNotFoundException(string message) : base("Blob Not Found !")
+        {
+        }
+
+        public BlobNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public BlobNotFoundException(RequestResult res, string message, Exception inner) : base(res, message, inner)
+        {
+        }
+
+        protected BlobNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
